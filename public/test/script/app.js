7d28ccae79fb01017c8d3b7b09cd3546826cb496
spadefaith <cedrickcampoto@yahoo.com>
2021-09-09 11:33:57 +0800

    changes

--- public/test/script/app.js
+++ public/test/script/app.js
@@ -1,42 +1,60 @@
+// let com2 = angular.module('com2', []);
+
 let cks = {
     form:`<h6>Registration Form</h6>
     <input placeholder=email class='input'>
-    <input placeholder=password class='input'>`,
+    <input placeholder=password class='input'>
+    <button ng-click='log()'>Click</button>
+    <button ng-click='close()'>Close</button>
+    `,
 };
-// let com1 = angular.module('com1',[]);
-
-// com1.factory('sat', function(){
-//     return {sat:[]};
-// })
-
-// com1.controller('c1-control', ['$scope', '$compile', function($scope, $compile, $attrs){
-//     console.log($attrs);
-//     $scope.name = 'james';
-//     let html = `<p ng-click="alert('clicked')" is-test=true>Hello World</p>`;
-//     let compile = $compile(html);
-//     let link = compile($scope);
-//     angular.element(document.querySelector('[ng-controller]')).append(link);
-// }]);
-
-// let c1 = document.createElement('div');
-// c1.setAttribute('ng-controller','c1-control');
-// c1.setAttribute('is-test','true');
 
 
+function mng(){}
 
+let com1 = angular.module('com1',['mng']);
 
-// function createComponent(name, template){
-    
-//     angular.element()
-// }
-
-
-let com1 = angular.module('com1',[]);
-
-com1.component('my-el', {
-    template:cks.form,
-    controller: function($scope, $compile, $attrs){
-        console.log($attrs);
+com1.controller('controller1', function($scope){
+    $scope.openForm = function(){
+        console.log('form is open');
+        // console.log(this);
+        $scope.$broadcast('openForm', {msg:'open the form'});
     }
-})
-// angular.bootstrap(document, ['com1']);
+});
+
+com1.controller('controller2', function($scope,$element, $window){
+    $scope.$on('openForm', function(event, args){
+        console.log(event, args);
+    });
+    $scope.log = function(){
+        console.log('log here');
+    };
+    $scope.close = function(){
+        console.log('close the component');
+        console.log($element, $window);
+        // this.$destroy();
+        // $element.empty();
+        $element[0].remove();
+    };
+});
+
+com1.directive('myEl', function(){
+    return {
+        scope:true,
+        restrict: 'A',
+        template:cks.form,
+        controller:'controller2'
+    }
+});
+
+
+
+
+let el = document.createElement('div')
+el.setAttribute('my-el', "");
+document.body.appendChild(el);
+
+
+function createComponent(name, template){
+
+};
